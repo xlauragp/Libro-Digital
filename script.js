caab2838b0a606302d2e4d3bf2b689b47bc34fb6
xlauragp <142696546+xlauragp@users.noreply.github.com>
2024-10-22 10:15:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-$(function() {
-    $('#mybook').booklet({
+$(document).ready(function() {
+    $("#flipbook").turn({
         width: 800,
-        height: 500,
+        height: 600,
         autoCenter: true,
-        closed: true,
-        pagePadding: 0,
-        pageNumbers: true
+        display: 'double',
+        elevation: 50,
+        gradients: true,
+        when: {
+            turned: function(e, page) {
+                $("#page-num").text(page);
+            }
+        }
+    });
+
+    // Total page count
+    $("#page-count").text($("#flipbook").turn("pages"));
+
+    // Previous page button
+    $("#prev-page").click(function() {
+        $("#flipbook").turn("previous");
+    });
+
+    // Next page button
+    $("#next-page").click(function() {
+        $("#flipbook").turn("next");
+    });
+
+    // Highlighting feature
+    $(document).mouseup(function() {
+        var selection = window.getSelection();
+        if (selection.toString().length > 0) {
+            var range = selection.getRangeAt(0);
+            var span = document.createElement("span");
+            span.className = "highlight";
+            span.appendChild(range.extractContents());
+            range.insertNode(span);
+        }
     });
 });
