6a705d2753dbd1c5b1feefa7475d8d033c391982
xlauragp <142696546+xlauragp@users.noreply.github.com>
2024-10-22 12:36:06 -0700

    capitulos

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(function() {
     $('#mybook').booklet({
-        width: '100%',
-        height: 500,
+        width: 800,
+        height: 600,
         auto: false,
         closed: true,
         covers: true,
         pagePadding: 10
+    });
+
+    $('#chapter-select').on('change', function() {
+        const selectedPage = $(this).val();
+        // Navegar a la página correspondiente
+        $('#mybook').booklet("gotopage", selectedPage);
     });
 });
