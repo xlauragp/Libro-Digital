eca095404936d4f969dc06fbcefb48528099223a
xlauragp <142696546+xlauragp@users.noreply.github.com>
2024-10-22 10:22:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-$(document).ready(function() {
-    $("#flipbook").turn({
+$(document).ready(function () {
+    // Inicializa el booklet para crear el efecto de libro
+    $('#mybook').booklet({
         width: 800,
-        height: 600,
+        height: 500,
         autoCenter: true,
-        display: 'double',
-        elevation: 50,
-        gradients: true,
-        when: {
-            turned: function(e, page) {
-                $("#page-num").text(page);
-            }
-        }
-    });
-
-    // Total page count
-    $("#page-count").text($("#flipbook").turn("pages"));
-
-    // Previous page button
-    $("#prev-page").click(function() {
-        $("#flipbook").turn("previous");
-    });
-
-    // Next page button
-    $("#next-page").click(function() {
-        $("#flipbook").turn("next");
-    });
-
-    // Highlighting feature
-    $(document).mouseup(function() {
-        var selection = window.getSelection();
-        if (selection.toString().length > 0) {
-            var range = selection.getRangeAt(0);
-            var span = document.createElement("span");
-            span.className = "highlight";
-            span.appendChild(range.extractContents());
-            range.insertNode(span);
-        }
+        pageNumbers: true,
+        arrows: false
     });
 });
